efb4ee683e990cfc8cb52322e7eafe1601af5255
ghebyon <89339517+ghebyon@users.noreply.github.com>
2021-10-01 07:25:36 +0700

    Update Main.java
    Belum di implementasikan from to file dan karna check belum sempurna(gauss dan gauss jordan belum sepenuhnya selesai)

--- src/com/tubes/Main.java
+++ src/com/tubes/Main.java
@@ -1,43 +1,144 @@
 package com.tubes;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main { 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
         int inputMain;
+        int baris, kolom;
         do{
             inputMain =  getMainOption();
             if(inputMain == 1){
                 int optionSPL = getSPLOption();
                 if(optionSPL == 1){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        MatrixSPL M = new MatrixSPL(baris, kolom);
+                        M.inputMat();
+                        primMatrix tempMatrix = M.gauss();
+                        M.matrix = tempMatrix.matrix;
+                        M.check();
+                        M.displaySolution(M.ExactSolution());
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }else if(optionSPL == 2){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        MatrixSPL M = new MatrixSPL(baris, kolom);
+                        M.inputMat();
+                        primMatrix tempMatrix = M.gaussJordan();
+                        M.matrix = tempMatrix.matrix;
+                        M.check();
+                        M.displaySolution(M.ExactSolution());
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }else if(optionSPL == 3){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        MatrixSPL M = new MatrixSPL(baris, kolom);
+                        M.inputMat();
+                        primMatrix tempMatrix = M.metodeInvers();
+                        M.matrix = tempMatrix.matrix;
+                        M.checkII();
+                        if(M.noSolution){
+                            M.InvalidSolution();
+                        }else{
+                            M.displaySolution(M.ExactSolutionV2());
+                        }
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }else if(optionSPL == 4){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        MatrixSPL M = new MatrixSPL(baris, kolom);
+                        M.inputMat();
+                        primMatrix tempMatrix = M.metodeInvers();
+                        M.matrix = tempMatrix.matrix;
+                        M.checkII();
+                        if(M.noSolution){
+                            M.InvalidSolution();
+                        }else{
+                            M.displaySolution(M.ExactSolutionV2());
+                        }
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }
             }else if(inputMain == 2){
                 int optionDeterminan = getDeterminanOption();
                 if(optionDeterminan == 1){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        primMatrix M = new primMatrix(baris, kolom);
+                        M.inputMat();
+                        System.out.printf("Determinan Matriks : %f", M.determinanOBE());
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }else if(optionDeterminan == 2){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        primMatrix M = new primMatrix(baris, kolom);
+                        M.inputMat();
+                        System.out.printf("Determinan Matriks : %f", M.determinanKofaktor(M));
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }
             }else if(inputMain == 3){
                 int optionInverse = getInverseOption();
                 if(optionInverse == 1){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        MatrixInverse M = new MatrixInverse(baris, kolom);
+                        M.inputMat();
+                        primMatrix tempMatrix = M.InversOBE();
+                        tempMatrix.displayMatrix();
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }else if(optionInverse == 2){
-                    int inputMethod = metodeInput();
+                    int inputMetode = metodeInput();
+                    if(inputMetode == 1){
+                        baris = inputBaris();
+                        kolom = inputBaris();
+                        MatrixInverse M = new MatrixInverse(baris, kolom);
+                        M.inputMat();
+                        primMatrix tempMatrix = M.inversCofac();
+                        tempMatrix.displayMatrix();
+                    }else if(inputMetode == 2){
+                        
+                    }
                 }
             }else if(inputMain == 4){
-                int inputMethod = metodeInput();
+                int inputMetode = metodeInput();
             }else if(inputMain == 5){
-                int inputMethod = metodeInput();
+                int inputMetode = metodeInput();
             }else if(inputMain == 6){
                 System.out.print("\033[H\033[2J"); 
                 System.out.flush();  
+            }
+            if(inputMain != 6){
+                System.out.printf("Tekan <Enter> untuk melanjutkan");
+                System.in.read();
             }
         }while(inputMain != 6);
     }
@@ -59,7 +160,7 @@ public class Main {
         System.out.println("===================================================");
         System.out.printf("pilih [1-6]\t: ");
         i = input.nextInt();
-        input.close();
+
         return i;
     }
 
@@ -68,7 +169,7 @@ public class Main {
         System.out.flush();  
         int i;
         Scanner input = new Scanner(System.in);
-        System.out.println("SISTEM PERSAMAAN LINIER\n");
+        System.out.println("SISTEM PERSAMAAN LINIER");
         System.out.println("===================================================");
         System.out.println("1. Metode Eliminasi Gauss");
         System.out.println("2. Metode Eliminasi Gauss-JordaN");
@@ -77,7 +178,6 @@ public class Main {
         System.out.println("===================================================");
         System.out.printf("pilih [1-4]\t: ");
         i = input.nextInt();
-        input.close();
         return i;
     }
 
@@ -86,6 +186,7 @@ public class Main {
         System.out.flush();  
         int i;
         Scanner input = new Scanner(System.in);
+        System.out.println("\u000C");
         System.out.println("DETERMINAN\n");
         System.out.println("===================================================");
         System.out.println("1. Reduksi Baris");
@@ -93,7 +194,6 @@ public class Main {
         System.out.println("===================================================");
         System.out.printf("pilih [1-2]\t: ");
         i = input.nextInt();
-        input.close();
         return i;
     }
     private static int getInverseOption(){
@@ -108,7 +208,6 @@ public class Main {
         System.out.println("===================================================");
         System.out.printf("pilih [1-2]\t: ");
         i = input.nextInt();
-        input.close();
         return i;
     }
 
@@ -120,7 +219,19 @@ public class Main {
         System.out.println("2. Input file");
         System.out.printf("pilih [1-2]\t: ");
         i = input.nextInt();
-        input.close();
         return i;
+    }
+    
+    static int inputBaris(){
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Masukkan Banyak Baris : ");
+        int brs = input.nextInt();
+        return brs;
+    }
+    static int inputKolom(){
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Masukkan Banyak Kolom : ");
+        int klm = input.nextInt();
+        return klm;
     }
 }
